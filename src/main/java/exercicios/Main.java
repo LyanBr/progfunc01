6da2ed003fb4d9541497f61127ed4e22193d0e25
LyanBr <128609485+LyanBr@users.noreply.github.com>
2024-03-13 15:36:42 -0300

    Create Main.java

---
+++ src/main/java/exercicios/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        // Exemplo de uso
+        CidadeRecord cidadeRecord = new CidadeRecord("São Paulo");
+        Cidade cidade = new Cidade("Rio de Janeiro");
+
+        PessoaRecordShallow pessoaShallow = new PessoaRecordShallow("João", cidade);
+        PessoaRecord pessoa = new PessoaRecord("Maria", cidadeRecord);
+
+        // Testando os valores
+        System.out.println("Pessoa shallow: " + pessoaShallow);
+        System.out.println("Pessoa: " + pessoa);
+    }
+}
